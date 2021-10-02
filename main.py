b984cd3eb3bd500f847a06450dc4e5ab748a14d7
elmi-elmi <shahrokhelmi@gmail.com>
2021-10-02 23:46:11 +0330

    last commit

--- main.py
+++ main.py
@@ -20,11 +20,41 @@ def filter_data(rows, contains):
         if contains in row[0]:
             yield row
 
+
 data = parse_data('cars.csv')
-filtered=filter_data(data, 'Chevrolet')
+filter_1 = filter_data(data, 'Chevrolet')
+filter_2 = filter_data(filter_1, 'Carlo')
+
 print('-------------------')
-for row in itertools.islice(filtered, 5):
+for row in itertools.islice(filter_1, 5):
+    print(row)
+print('-------------------')
+for row in itertools.islice(filter_2, 5):
     print(row)
 
+def output(f_name):
+    data = parse_data(f_name)
+    filter_1 = filter_data(data, 'Chevrolet')
+    filter_2 = filter_data(filter_1, 'Carlo')
+    yield from filter_2
 
+result = output('cars.csv')
+print('----------------------')
+for row in result:
+    print(row)
 
+def output(f_name, *filter_words):
+    data = parse_data(f_name)
+    for filter_word in filter_words:
+        data = filter_data(data, filter_word)
+    yield from data
+
+print('===========================')
+result = output('cars.csv', 'Chevrolet')
+for row in itertools.islice(result,5):
+    print(row)
+
+print('===========================')
+result = output('cars.csv', 'Chevrolet','Carlo', 'Landau')
+for row in itertools.islice(result, 5):
+    print(row)
